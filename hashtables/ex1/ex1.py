ed23847cf0a240ae0f28e1a0a6b7da5e1464cd69
AbdelI <homeworkoracle@protonmail.com>
2020-05-08 11:16:58 +0200

    getindicesofitemweight, completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,25 +1,22 @@
 import sys
 
 from hashtable import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                       hash_table_insert,
+                       hash_table_remove,
+                       hash_table_retrieve,
+                       hash_table_resize)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-	ht = HashTable(16)
+    ht = HashTable(8)
 
-	for i in range(length):
-		looking = limit - weights[i]
-		found = hash_table_retrieve(ht, looking)
+    for i in range(length):
+        findindices = limit - weights[i]
+        indicefound = hash_table_retrieve(ht, findindices)
 
-		if found == None:
-			hash_table_insert(ht, weights[i], i)
-		else:
-			return max(found, i), min(found, i)
+        if indicefound == None:
+            hash_table_insert(ht, weights[i], i)
+        else:
+            return max(indicefound, i), min(indicefound, i)
 
-	return None
-
-
-#d
+    return None

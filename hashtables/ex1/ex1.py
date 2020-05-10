d14b8e94166cb26c6a1c605f8bf34d9158c9fa38
AbdelI <homeworkoracle@protonmail.com>
2020-05-10 16:50:25 +0200

    ex2, done, tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,7 +8,7 @@ from hashtable import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    # instance of dictioanry
+    # instance of dictionary
     dictio = {}
 # iterate over the list of weights
     for i, weight in enumerate(weights):
@@ -16,14 +16,11 @@ def get_indices_of_item_weights(weights, length, limit):
         if weight in dictio:
             # if current weight is in the dictionary,get its index by getting the value through the key of weight
             value = dictio[weight]
-            # return index and value wihtin tuple
+            # return current index and value(index in dictionary) wihtin tuple
             return (i, value)
             # if the weights arent in the dictionary,assign the difference key, the value of the current index
         else:
             diff = limit - weight
             dictio[diff] = i
 
-# There is going to be a limit
-# there is going to be a list of weights
-# the goal is to find the indexes of two items summed together that would equal the limit
-# the heaviest item should be placed first in the response
+
